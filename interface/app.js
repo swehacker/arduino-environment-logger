615764f52b6b7c6c3ea909df550daa9219b194e1
Patrik Falk <patrik@patrikfalk.com>
2015-02-06 01:06:25 +0100

    Added calculation for Lux

--- interface/app.js
+++ interface/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Modules
 var express = require('express');
 var app = express();
--- temp-logger/temp-logger.ino
+++ temp-logger/temp-logger.ino
@@ -3,7 +3,7 @@
  * temp.logger:
  *
  * When the arduino receives a newline character it will respond with temperature, humidity and heat index.
- * 
+ *
  * <temperature in celsius>,<humidity in percent>,<heat index in celsius>\n
  * e.g: 36.00,22.00,24.86
  */
@@ -21,11 +21,11 @@ aREST rest = aREST();
 int temperature;
 int humidity;
 int heatindex;
-int light;
+float light;
 
-void setup() {  
+void setup() {
   Serial.begin(115200);
-  
+
   // Expose variables to rest
   rest.variable("temperature", &temperature);
   rest.variable("humidity", &humidity);
@@ -34,31 +34,36 @@ void setup() {
   // Set devicename and id
   rest.set_id("1");
   rest.set_name("temperature_station");
-  
+
   // Setup DHT
   dht.begin();
 }
 
-void loop() {  
+void loop() {
   humidity = (int)dht.readHumidity();
   // Read temperature as Celsius
   temperature = (int)dht.readTemperature();
   // Read temperature as Fahrenheit
   float temp_f = dht.readTemperature(true);
-  
+
   // Check if any reads failed and exit early (to try again).
   if (isnan(humidity) || isnan(temperature) || isnan(temp_f)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
-  
+
   // Compute heat index
   // Must send in temp in Fahrenheit!
   heatindex = dht.computeHeatIndex(temp_f, humidity);
-  
+
+  // Calculate the lux (amount of ligth) (assuming a typical LDR)
+  // Volt = 5V, R = 10k
+  // Typical LDR: Ea= 10 lux, Ra = 10 k, sens = 0.8 (check datasheet)
   float light_reading = analogRead(LIGHTPIN);
-  light = (int)(light_reading/1024*100);
-  
+  float vOut = 5 * light_reading/1024;
+  float rLDR = 10.0 * (5.0 / vOut - 1.0);
+  float light 16.0 * pow(10.0 / rLDR, 1.0/0.8)
+
   Serial.print("{\"id\": 1,\"temperature\": ");
   Serial.print(temperature);
   Serial.print(",\"humidity\": ");
@@ -68,7 +73,6 @@ void loop() {
   Serial.print(",\"light\": ");
   Serial.print(light);
   Serial.println("}");
-  
+
   delay(1000);
 }
-
